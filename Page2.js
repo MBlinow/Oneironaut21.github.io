eaa8f83affda750d03302751bd58ee8a8659032e
Oneironaut21 <mblinow@gmail.com>
2017-03-05 19:22:08 -0500

    Functioning

--- Page2.js
+++ Page2.js
@@ -1,20 +1,34 @@
+
 //Variables
 	money=0.00;
+	moneyPerClick=1;
+	moneyPerTick=0.1;
 
 $(document).ready(function(){
 	
 
-		moneyPerClick=1;
-		$('.money').click(function(){;
-			
-			//money=tick();
+	//	moneyPerClick=1;
+		$('.money').click(function(){
+			moneyClick();
+
 		});
 		setInterval('tick()', 1000);
 });
 
 function tick(){
 	//$('.pageName').append('hi');
-	money+=0.1;
-	$('.money').text('$'+money.toFixed(2));
+	money+=moneyPerTick;
+	moneyUpdate();
+	//$('.money').text('$'+money.toFixed(2));
+	
 
-};
+	}
+	
+function moneyUpdate(){
+		$('.money').text('$'+money.toFixed(2));
+	}
+
+
+function moneyClick(){
+		money+=moneyPerClick;
+		moneyUpdate();}
